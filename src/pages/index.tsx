47c51291a1336a1784a392407264f3113b85c627
berilo queiroz <luiz.berilo@buson.com.br>
2023-01-26 22:28:02 -0300

    alterado favicon e video

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <title>bid</title>
         <meta name='description' content='bid site' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' href='/favicon.svg' />
       </Head>
       <Header />
       <main className={styles.main}>
