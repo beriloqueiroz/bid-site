64377f5da67116e89b16be9a50d3fe317f4f0a12
berilo queiroz <luiz.berilo@buson.com.br>
2023-01-27 11:57:22 -0300

    inserindo tag do ga4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import Presentation from "@/components/presentation";
 import About from "@/components/about";
 const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
+  const GA_TRACKING_ID = "G-BDL6T80L70";
   return (
     <>
       <Head>
@@ -15,6 +16,23 @@ export default function Home() {
         <meta name='description' content='bid site' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.svg' />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
       </Head>
       <Header />
       <main className={styles.main}>
