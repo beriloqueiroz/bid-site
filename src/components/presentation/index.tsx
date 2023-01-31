fe7e344a49a3ce9cce50551201b443d39c434f6c
berilo queiroz <luiz.berilo@buson.com.br>
2023-01-30 22:24:24 -0300

    lazyload video

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -1,5 +1,7 @@
 import styles from "@/components/presentation/style.module.scss";
 import { useRouter } from "next/router";
+import LazyLoad from "react-lazy-load";
+
 export default function Presentation() {
   const router = useRouter();
 
@@ -11,15 +13,17 @@ export default function Presentation() {
           <br /> <span>Simples assim!</span>
         </h1>
         <div className={styles.banner}>
-          <video
-            title='Apresentação da bid'
-            muted
-            autoPlay
-            loop
-            playsInline
-            preload='auto'>
-            <source src='/short_presentation.mp4' type='video/mp4' />
-          </video>
+          <LazyLoad>
+            <video
+              title='Apresentação da bid'
+              muted
+              autoPlay
+              loop
+              playsInline
+              preload='auto'>
+              <source src='/short_presentation.mp4' type='video/mp4' />
+            </video>
+          </LazyLoad>
         </div>
         <p className={styles.presentationDescription}>
           A bid é entrega sem complicação, de forma <strong>rápida</strong> e{" "}
